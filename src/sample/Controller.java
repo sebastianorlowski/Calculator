4b2abbe3381cdeecf4ef3a89da6cb7600c47cee4
mydarkside <sebekovsky@gmail.com>
2020-02-27 19:16:35 +0100

    Calculator Interface

---
+++ src/sample/Controller.java
@@ -0,0 +1,68 @@
+package sample;
+
+public class Controller {
+
+    public void clickButtonOne() {
+
+    }
+
+    public void clickButtonTwo() {
+
+    }
+
+    public void clickButtonThree() {
+
+    }
+
+    public void clickButtonFour() {
+
+    }
+
+    public void clickButtonFive() {
+
+    }
+
+    public void clickButtonSix() {
+
+    }
+
+    public void clickButtonSeven() {
+
+    }
+
+    public void clickButtonEight() {
+
+    }
+
+    public void clickButtonNine() {
+
+    }
+
+    public void clickButtonZero() {
+
+    }
+
+    public void clickButtonAdd() {
+
+    }
+
+    public void clickButtonSubtract() {
+
+    }
+
+    public void clickButtonMultiply() {
+
+    }
+
+    public void clickButtonDivide() {
+
+    }
+
+    public void clickButtonClear() {
+
+    }
+
+    public void clickButtonEquals() {
+
+    }
+}
